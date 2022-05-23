e5508581c1d93bdbadf14d008f975845106a406c
hemantth <hemantth@cybage.com>
2022-05-23 15:45:26 +0530

    Initial commit

---
+++ publicsdk/src/main/java/com/sdk/publicsdk/Arithmetic.kt
@@ -0,0 +1,24 @@
+package com.sdk.publicsdk
+
+/**
+ * Created by Hemant Thakare on 5/23/2022.
+ */
+class Arithmetic {
+
+    fun addition(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun subtraction(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    fun multiplication(a: Int, b: Int): Int {
+        return a * b
+    }
+
+    fun division(a: Int, b: Int): Int {
+        return a / b
+    }
+
+}
